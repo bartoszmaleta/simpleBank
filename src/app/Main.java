0ead5025a0de60b193f76d09404f4071f23747d4
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-23 13:23:55 +0100

    Testing in Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,21 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Bank bank = new Bank("Worldwide Programmers Bank");
+        bank.addBranch("Utopia");
+        bank.addCustomer("Utopia", "Bartosz", 10.45);
+        bank.addCustomer("Utopia", "John", 99.75);
+        bank.addCustomer("Utopia", "Tom", 128.64);
+
+        bank.addBranch("Atlantis");
+        bank.addCustomer("Atlantis", "Steven", 512.16);
+
+        bank.addCustomerTransaction("Utopia", "Bartosz", 4569.78);
+        bank.addCustomerTransaction("Utopia", "Bartosz", 1047.66);
+        bank.addCustomerTransaction("Utopia", "John", 645.78);
+        // bank.addCustomerTransaction("Utopia", "Bartosz", 45.78);
+
+        bank.listCustomers("Utopia", false);
+
     }
 }
